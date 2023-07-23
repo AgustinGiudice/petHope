8784e484a019622ba9230d08a07f0fb37daacf51
Alexis J Maubert <alexismaubertop@gmail.com>
2023-07-23 18:10:51 -0300

    FIx menu

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import CustomComponent from "./components/CustomComponent";
 import HamburguerMenu from "./components/HamburguerMenu";
+import Menu from "./components/Menu";
 
 export default function App() {
   return (
