93839ec9eb495be1c3e589dac603932ea06f1073
Agustin Giudice <agustin.giudice2@gmail.com>
2023-08-05 20:45:40 -0300

    registerpet

--- screens/RegisterPet.js
+++ screens/RegisterPet.js
@@ -1,20 +1,15 @@
 import React, { useState } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-} from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Picker, Switch, StyleSheet } from "react-native";
 
 const RegisterPet = ({ navigation }) => {
   const initialPetData = {
     nombre: "",
-    edad: "",
-    nivelCuidado: "",
-    tamanio: "",
+    edad: "cachorro", 
+    nivelCuidado: "1", 
+    cuidadosEspeciales: false,
+    raza: "perro", 
+    tamanio: "chico", 
     pic: "",
-    raza: "",
   };
   const [petData, setPetData] = useState(initialPetData);
 
@@ -47,51 +42,86 @@ const RegisterPet = ({ navigation }) => {
       >
         <Text style={styles.goBackText}>Volver</Text>
       </TouchableOpacity>
-      <Text style={styles.label}>Nombre:</Text>
-      <TextInput
-        style={styles.input}
-        value={petData.nombre}
-        onChangeText={(value) => setPetData({ ...petData, nombre: value })}
-        placeholder="Nombre"
-      />
-      <Text style={styles.label}>Edad:</Text>
-      <TextInput
-        style={styles.input}
-        value={petData.edad}
-        onChangeText={(value) => setPetData({ ...petData, edad: value })}
-        placeholder="Edad"
-      />
-      <Text style={styles.label}>Nivel de Cuidado:</Text>
-      <TextInput
-        style={styles.input}
-        value={petData.nivelCuidado}
-        onChangeText={(value) =>
-          setPetData({ ...petData, nivelCuidado: value })
-        }
-        placeholder="Nivel de cuidado"
-        keyboardType="numeric"
-      />
-      <Text style={styles.label}>Tamaño:</Text>
-      <TextInput
-        style={styles.input}
-        value={petData.tamanio}
-        onChangeText={(value) => setPetData({ ...petData, tamanio: value })}
-        placeholder="Tamaño"
-      />
-      <Text style={styles.label}>Raza:</Text>
-      <TextInput
-        style={styles.input}
-        value={petData.raza}
-        onChangeText={(value) => setPetData({ ...petData, raza: value })}
-        placeholder="Tamaño"
-      />
-      <Text style={styles.label}>Pic:</Text>
-      <TextInput
-        style={styles.input}
-        value={petData.pic}
-        onChangeText={(value) => setPetData({ ...petData, pic: value })}
-        placeholder="Pic"
-      />
+
+      <View style={styles.fieldContainer}>
+        <Text style={styles.label}>Nombre:</Text>
+        <TextInput
+          style={styles.input}
+          value={petData.nombre}
+          onChangeText={(value) => setPetData({ ...petData, nombre: value })}
+          placeholder="Nombre"
+        />
+      </View>
+
+      <View style={styles.fieldContainer}>
+        <Text style={styles.label}>Edad:</Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={petData.edad}
+          onValueChange={(itemValue) => setPetData({ ...petData, edad: itemValue })}
+        >
+          <Picker.Item label="Cachorro" value="cachorro" />
+          <Picker.Item label="Juvenil" value="juvenil" />
+          <Picker.Item label="Adulto" value="adulto" />
+        </Picker>
+      </View>
+
+      <View style={styles.fieldContainer}>
+        <Text style={styles.label}>Nivel de Cuidado:</Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={petData.nivelCuidado}
+          onValueChange={(itemValue) => setPetData({ ...petData, nivelCuidado: itemValue })}
+        >
+          <Picker.Item label="1" value="1" />
+          <Picker.Item label="2" value="2" />
+          <Picker.Item label="3" value="3" />
+        </Picker>
+      </View>
+
+      <View style={styles.fieldContainer}>
+        <Text style={styles.label}>Requiere cuidados especiales?</Text>
+        <Switch
+          value={petData.cuidadosEspeciales}
+          onValueChange={(value) => setPetData({ ...petData, cuidadosEspeciales: value })}
+        />
+      </View>
+
+      <View style={styles.fieldContainer}>
+        <Text style={styles.label}>Raza:</Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={petData.raza}
+          onValueChange={(itemValue) => setPetData({ ...petData, raza: itemValue })}
+        >
+          <Picker.Item label="Perro" value="perro" />
+          <Picker.Item label="Gato" value="gato" />
+        </Picker>
+      </View>
+
+      <View style={styles.fieldContainer}>
+        <Text style={styles.label}>Tamaño:</Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={petData.tamanio}
+          onValueChange={(itemValue) => setPetData({ ...petData, tamanio: itemValue })}
+        >
+          <Picker.Item label="Chico" value="chico" />
+          <Picker.Item label="Medio" value="medio" />
+          <Picker.Item label="Grande" value="grande" />
+        </Picker>
+      </View>
+
+      <View style={styles.fieldContainer}>
+        <Text style={styles.label}>Pic:</Text>
+        <TextInput
+          style={styles.input}
+          value={petData.pic}
+          onChangeText={(value) => setPetData({ ...petData, pic: value })}
+          placeholder="Pic"
+        />
+      </View>
+
       <TouchableOpacity style={styles.button} onPress={handleSubmit}>
         <Text style={styles.buttonText}>Guardar</Text>
       </TouchableOpacity>
@@ -104,6 +134,9 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: "#fff",
   },
+  fieldContainer: {
+    marginBottom: 15,
+  },
   label: {
     fontSize: 16,
     fontWeight: "bold",
@@ -115,7 +148,12 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 5,
     paddingHorizontal: 10,
-    marginBottom: 15,
+  },
+  picker: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
   },
   button: {
     backgroundColor: "blue",
@@ -125,6 +163,18 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  goBackButton: {
+    backgroundColor: "gray",
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    alignItems: "center",
+  },
+  goBackText: {
     color: "#fff",
     fontSize: 16,
     fontWeight: "bold",
