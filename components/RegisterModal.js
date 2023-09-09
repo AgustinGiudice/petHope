1389af43d804dd39dca0da9a4b9d3efdb135859b
Alexis J Maubert <alexismaubertop@gmail.com>
2023-09-09 19:50:10 -0300

    Animacion de los modales

--- components/RegisterModal.js
+++ components/RegisterModal.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
-import { Modal, StyleSheet, View, Text } from "react-native";
+import { useState, useEffect } from "react";
+import { Modal, StyleSheet, Text, Animated, Easing } from "react-native";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 const RegisterModal = ({ children, visible, setVisible }) => {
   const [error, setError] = useState([]);
+  const modalFadeIn = new Animated.Value(0);
+  const modalFadeOut = new Animated.Value(1);
 
   const handleSubmit = () => {
     let hasEmptyValues = false;
@@ -35,14 +37,49 @@ const RegisterModal = ({ children, visible, setVisible }) => {
       setVisible((prev) => prev + 1);
     }
   };
+
+  useEffect(() => {
+    if (visible) {
+      Animated.timing(modalFadeIn, {
+        toValue: 1,
+        duration: 300, // Duración de la animación en milisegundos
+        easing: Easing.linear, // Tipo de interpolación
+        useNativeDriver: true, // Habilita el uso del controlador nativo para el rendimiento
+      }).start();
+    } else {
+      Animated.timing(modalFadeOut, {
+        toValue: 0,
+        duration: 300,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      });
+    }
+  }, [visible]);
+
   return (
     <Modal
-      animationType="fade"
       transparent={false}
       visible={visible}
       onRequestClose={() => setVisible((prev) => prev - 1)}
     >
-      <View style={styles.modal}>
+      <Animated.View
+        style={[
+          styles.modal,
+          {
+            opacity: visible ? modalFadeIn : modalFadeOut,
+            transform: [
+              {
+                scale: visible
+                  ? modalFadeIn.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0.7, 1],
+                    })
+                  : modalFadeOut,
+              },
+            ],
+          },
+        ]}
+      >
         {children}
         {error.length !== 0
           ? error.map((e) => {
@@ -59,7 +96,7 @@ const RegisterModal = ({ children, visible, setVisible }) => {
           style={styles.arrow}
           onPress={() => handleSubmit()}
         />
-      </View>
+      </Animated.View>
     </Modal>
   );
 };
