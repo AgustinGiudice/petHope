154607e0bef1aeaffc301fdb59932ef67e7b8de7
Marcos Lacoa <marcos.lacoa@davinci.edu.ar>
2023-10-02 17:42:02 -0300

    cambio

--- screens/tabScreens/MorePersonalData.js
+++ screens/tabScreens/MorePersonalData.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Image,TextInput  } from "react-native";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 import { screenHeight, screenWidth } from "../../hooks/useScreenResize";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
@@ -10,14 +10,22 @@ import { color } from "react-native-reanimated";
 
 const MorePersonalData = ({ navigation }) => {
   const [modalVisible, setModalVisible] = useState(false);
-  const { currentUser } = useContext(UserContext);
+  const { currentUser, setCurrentUser } = useContext(UserContext);
   const userData = currentUser;
-  currentUser.edad = 30;
-  currentUser.completado = 100;
-  currentUser.descripcion =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elit ligula, tincidunt quis ante at, bibendum placerat odio. Mauris eget tristique nunc. Aliquam posuere erat pellentesque cursus semper. Nam id mauris nec lectus rhoncus blandit a a sapien. ";
+  console.log(userData);
+  
   currentUser.pic = null;
   //pic: "https://images.pexels.com/photos/5648357/pexels-photo-5648357.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+  const [editableData, setEditableData] = useState({
+    nombre: userData.nombre,
+    apellido: userData.apellido,
+    edad: String(userData.edad),
+    mail: userData.mail,
+    telefono: userData.telefono,
+    descripcion: userData.descripcion,
+  });
+
   const [profilePic, setProfilePic] = useState(userData.pic);
   const [error, setError] = useState(null);
   useEffect(() => {});
@@ -28,8 +36,34 @@ const MorePersonalData = ({ navigation }) => {
   };
 
   const handleEditInformation = () => {
-    setError("Todavía no podés cambiar tu información");
+    const updatedUserData = {
+      ...editableData,
+      edad: Number(editableData.edad),  // Convertir la edad de nuevo a número
+      telefono : Number(editableData.telefono)
+    };
+  
+    fetch(`https://mascotas-back-31adf188c4e6.herokuapp.com/api/usuarios/edit/${userData.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedUserData)
+    })
+    .then(response => response.json())
+    .then(data => {
+    
+      // actualizar el usuario en el contexto
+      setCurrentUser(data.usuario);
+
+      navigation.navigate("PersonalData");
+    })
+    .catch(error => {
+      console.error("Error actualizando la información:", error);
+      setError("Error al actualizar la información");
+    });
   };
+  
+  
   return (
     <>
       <View style={styles.container}>
@@ -80,40 +114,78 @@ const MorePersonalData = ({ navigation }) => {
           )}
         </View>
         <View style={styles.dataContainer}>
-          <TouchableOpacity
-            onPress={() => handleEditInformation()}
-            style={styles.changePic}
-          >
-            <FontAwesome5
-              name="user-edit"
-              size={20}
-              style={{ color: "#9A34EA" }}
+  <TouchableOpacity
+    onPress={() => handleEditInformation()}
+    style={styles.changePic}
+  >
+    <FontAwesome5
+      name="user-edit"
+      size={20}
+      style={{ color: "#9A34EA" }}
+    />
+  </TouchableOpacity>
+  {error && <Text style={{ color: "red" }}>{error}</Text>}
+  <View style={styles.column}>
+    <View style={styles.textContainer}>
+      <Text style={styles.fieldName}>Nombre</Text>
+      <TextInput
+        value={editableData.nombre}
+        onChangeText={(text) => setEditableData(prevState => ({ ...prevState, nombre: text }))}
+        style={styles.inputStyle}
+        placeholder="Nombre"
+      />
+    </View>
+    <View style={styles.textContainer}>
+      <Text style={styles.fieldName}>Apellido</Text>
+      <TextInput
+        value={editableData.apellido}
+        onChangeText={(text) => setEditableData(prevState => ({ ...prevState, apellido: text }))}
+        style={styles.inputStyle}
+        placeholder="Apellido"
+      />
+    </View>
+    <Text style={styles.fieldName}>Edad</Text>
+    <View style={styles.textContainer}>
+      <TextInput 
+        value={editableData.edad}
+        onChangeText={(text) => setEditableData(prevState => ({ ...prevState, edad: text }))}
+        style={styles.inputStyle}
+        placeholder="Edad"
+        keyboardType="numeric"
+      />
+    </View>
+    <Text style={styles.fieldName}>Teléfono</Text>
+    <View style={styles.textContainer}>
+      <TextInput
+        value={editableData.telefono}
+        onChangeText={(text) => setEditableData(prevState => ({ ...prevState, telefono: text }))}
+        style={styles.inputStyle}
+        placeholder="Teléfono"
+        keyboardType="numeric"
+      />
+    </View>
+    <Text style={styles.fieldName}>E-mail</Text>
+    <View style={styles.textContainer}>
+            <TextInput 
+              value={editableData.mail}
+              onChangeText={(text) => setEditableData(prevState => ({ ...prevState, mail: text }))}
+              style={styles.inputStyle}
+              placeholder="E-mail"
+              keyboardType="email-address"
             />
-          </TouchableOpacity>
-          {error && <Text style={{ color: "red" }}>{error}</Text>}
-          <View style={styles.column}>
-            <View style={styles.textContainer}>
-              <Text style={styles.fieldName}>Nombre</Text>
-              <Text style={styles.textBlack}>
-                {userData.nombre} {userData.apellido}
-              </Text>
-            </View>
-            <Text style={styles.fieldName}>Edad</Text>
-            <View style={styles.textContainer}>
-              <Text style={styles.textBlack}>{userData.edad}</Text>
-            </View>
-            <Text style={styles.fieldName}>Teléfono</Text>
-            <View style={styles.textContainer}>
-              <Text style={styles.textBlack}>{userData.telefono}</Text>
-            </View>
-            <Text style={styles.fieldName}>E-mail</Text>
-            <View style={styles.textContainer}>
-              <Text numberOfLines={1} style={styles.textBlack}>
-                {userData.mail}
-              </Text>
-            </View>
-          </View>
-        </View>
+    </View>
+    <Text style={styles.fieldName}>Descripcion</Text>
+    <View style={styles.textContainer}>
+            <TextInput 
+              value={editableData.descripcion}
+              onChangeText={(text) => setEditableData(prevState => ({ ...prevState, descripcion: text }))}
+              style={styles.inputStyle}
+              placeholder="Descripcion"
+            />
+    </View>
+    
+  </View>
+</View>
       </View>
     </>
   );
@@ -221,6 +293,12 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: "bold",
     fontSize: 35,
+  },
+  inputStyle: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 5,
+    borderRadius: 5,
   },
 });
 
