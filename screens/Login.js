3ef240d446e521e4e632a4d4a334e764427998ad
Alexis J Maubert <alexismaubertop@gmail.com>
2023-10-26 16:49:51 -0300

    cambios en el fetch de login

--- screens/Login.js
+++ screens/Login.js
@@ -10,6 +10,7 @@ import Input from "../components/Input";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { UserContext } from "../context/UserContext";
 import { TokenContext } from "../context/TokenContext";
+import { login } from "../services/logIn";
 
 const LoginScreen = ({ navigation }) => {
   const [userData, setUserData] = useState({ mail: "", pass: "" });
@@ -19,61 +20,25 @@ const LoginScreen = ({ navigation }) => {
   const [isRefugio, setIsRefugio] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleLogin = () => {
+  const handleLogin = async () => {
     setIsLoading(true);
-    const url = isRefugio
-      ? "https://mascotas-back-31adf188c4e6.herokuapp.com/api/refugios/login"
-      : "https://mascotas-back-31adf188c4e6.herokuapp.com/api/usuarios/login";
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.token && data.usuario) {
-          data.usuario.imagen && data.usuario.tuvoMascotas
-            ? (data.usuario.completado = 100)
-            : data.usuario.imagen || data.usuario.tuvoMascotas
-            ? (data.usuario.completado = 66)
-            : (data.usuario.completado = 33);
-
-          const data_user = {
-            token: data.token,
-            usuario: data.usuario,
-            matches: data.matches,
-          };
-          setToken(data.token);
-          AsyncStorage.setItem("token", JSON.stringify(data_user))
-            .then(() => {
-              console.log("Token guardado correctamente:", data.token);
-              return AsyncStorage.getItem("token"); // Recuperar el token
-            })
-            .then((storedToken) => {
-              console.log("Token almacenado en AsyncStorage:", storedToken); //mostrar token en async storage
-              const { usuario, token } = data;
-              setCurrentUser(usuario);
-              //clean state
-              setUserData({ mail: "", pass: "" });
-              setError("");
-              navigation.navigate("Tabs", { usuario, token });
-            });
-        } else {
-          setError("Inicio de sesión fallido"); // Establecer mensaje de error
-        }
-      })
-      .catch((error) => {
-        console.error("Error al iniciar sesión:", error);
-      })
-      .finally(() => setIsLoading(false));
+    await login(
+      userData,
+      setUserData,
+      isRefugio,
+      setToken,
+      setCurrentUser,
+      setError,
+      navigation
+    );
+    setIsLoading(false);
   };
 
   useEffect(() => {
     const token = AsyncStorage.getItem("token");
     console.log(token);
   }, []);
+  
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Iniciar Sesión</Text>
