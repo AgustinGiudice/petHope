8577cc4c81c101d854ea6857a5f926a2fda42626
Alexis J Maubert <alexismaubertop@gmail.com>
2023-10-27 19:38:41 -0300

    cambios

--- screens/tabScreens/refugioScreens/RegisterPet.js
+++ screens/tabScreens/refugioScreens/RegisterPet.js
@@ -2,6 +2,9 @@ import React, { useContext, useEffect, useState } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import Input from "../../../components/Input";
 import Select from "../../../components/Select";
+import SelectDropdown from "react-native-select-dropdown";
+//https://github.com/AdelRedaa97/react-native-select-dropdown#buttonTextAfterSelection
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import { BASE_URL } from "@env";
 
 const RegisterPet = ({ navigation }) => {
@@ -33,10 +36,45 @@ const RegisterPet = ({ navigation }) => {
           placeholder="Perro o gato"
           atributo="animal"
         />
-        <Select
-          values={["Perro", "Gato"]}
-          setValue={setNewPetData}
-          atributo={"Selecciona tipo de animal"}
+        {/* <Select
+           values={["Perro", "Gato"]}
+           setValue={setNewPetData}
+           atributo={"Selecciona tipo de animal"}
+         /> */}
+
+        <SelectDropdown
+          data={["Perro", "Gato"]}
+          buttonStyle={{
+            borderColor: "#C69AE8",
+            borderWidth: 1,
+            borderRadius: 5,
+            backgroundColor: "#e3e3e3",
+            width: "80%",
+          }}
+          renderDropdownIcon={() => {
+            return (
+              <MaterialIcons
+                name="keyboard-arrow-down"
+                size={25}
+                style={{ color: "#C69AE8" }}
+              />
+            );
+          }}
+          buttonTextStyle={{
+            color: "#9A34EA",
+          }}
+          defaultButtonText="Selecciona que animal es"
+          rowStyle={{
+            borderColor: "#9A34EA",
+            borderRightWidth: 5,
+            borderLeftWidth: 5,
+            borderBottomColor: "#9A34EA",
+          }}
+          rowTextStyle={{ color: "#9A34EA" }}
+          selectedRowStyle={{ backgroundColor: "#C69AE8" }}
+          selectedRowTextStyle={{ color: "#e3e3e3" }}
+          dropdownOverlayColor="#rgba(50,50,50,0.8)"
+          dropdownStyle={{ margin: 0 }}
         />
         <Input
           value={newPetData.sexo}
